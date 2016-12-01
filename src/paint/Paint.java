8044250cdae5e43be8ff5d3bd90ef2323b307fcb
student <student@240-03.INFORMATYKA.local>
2016-12-01 10:04:36 +0100

    start

---
+++ src/paint/Paint.java
@@ -0,0 +1,16 @@
+package paint;
+
+/**
+ *
+ * @author student
+ */
+public class Paint {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
